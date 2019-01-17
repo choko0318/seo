04f1abbbdb6b111549572e9c08c9f578182d3e65
choko0318 <39220671+choko0318@users.noreply.github.com>
2019-01-17 19:51:18 +0900

    first
    sadf

---
+++ seo1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>
+    SeoDongHa
+  </title>
+  <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css">
+  <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
+  <link rel="stylesheet" href="seo1.css">
+ 
+</head>
+
+<body>
+  <div class="seo">
+      <div id="icon">
+          <img src="./img/aroow.png" style="width:80px; transform:rotate(180deg);">
+      </div>
+    <div id="page1">
+      <div class="page1-con">
+        <div class="tt">
+          <a>SeoDongHa</a>
+        </div>
+        <div id="adouttt">
+          17 Year-old Web Develop & Design
+        </div>
+        <div class="dimigo">
+          Degital Media High School
+        </div>
+        <div class="hkdf">
+          Hacking Defense 1 - 6
+        </div>
+      </div>
+    </div>
+    <div id="page2">
+      <div class="page2-con">
+        <h3>
+        CLUB
+      </h3>
+      </div>
+    </div>
+
+    <div id="page3">
+      <div class="page3-con">
+        <h3>TEDx</h3>
+        <div class="text">
+          <br> 디미고에서 일반동아리 강연 TED를 직접 주최하는 'TEDxYouth'에서 <br> 연사 & 강연 기획 담당 동아리원으로 활동하고 있습니다.
+        </div>
+      </div>
+    </div>
+    <div id="page4">
+      <div class="page4-con">
+        <h3>LUNA</h3>
+        <div class="text">
+          <br> 정기구독형 코딩교육 플랫폼을 중심으로 결성된 창업동아리 LUNA에서 <br> 기획과 웹 개발을 열심히 배우고 있습니다.
+        </div>
+      </div>
+    </div>
+    <div id="page5">
+      <div class="page5-con">
+        <h3>SULLIVAN</h3>
+        <div class="text">
+          <br>학생들이 코딩으로 만들고 싶은 것을 만들 수 있도록 돕기 위해 시작한<br> 고등학생 주도의 코딩 교육봉사 프로젝트에서 선생님으로 참여중입니다.
+        </div>
+      </div>
+    </div>
+</body>
+
+</html>
